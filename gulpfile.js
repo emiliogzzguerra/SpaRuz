91a9c817d8c4af314db06cfc64bc536c493a6f33
emiliogzzguerra <emiliogzz24@gmail.com>
2018-02-22 17:19:39 -0600

    initial setup, using npm to install dependencies but will consider switch to bower..

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+var config = {
+    bootstrapDir: './node_modules/bootstrap',
+    assetsDir: './assets',
+};
+
+//Setup for customization
+gulp.task('sass', function() {
+    return gulp.src(config.assetsDir + '/scss/landing-page.scss')
+    .pipe(sass({
+        includePaths: [config.bootstrapDir + '/scss'],
+    }))
+    .pipe(gulp.dest(config.assetsDir + '/css'));
+});
+
+// // Compile sass into CSS & auto-inject into browsers
+// gulp.task('sass', function() {
+//     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','assets/scss/*.scss'])
+//         .pipe(sass())
+//         .pipe(gulp.dest("assets/css"))
+//         .pipe(browserSync.stream());
+// });
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+        .pipe(gulp.dest("assets/js"))
+        .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"  
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'assets/scss/*.scss'], ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['sass','js', 'serve']);
