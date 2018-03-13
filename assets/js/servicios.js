2b9b27db27e5645e4c43c588b2321b01d3864f44
emiliogzzguerra <emiliogzz24@gmail.com>
2018-03-13 17:23:43 -0600

    icono se mueve al darle click a tarjeta

--- assets/js/servicios.js
+++ assets/js/servicios.js
@@ -1,10 +1,19 @@
-$('a[role="tab"]').click(function(e){
+$('.subdivision').click(function(e){
   e.preventDefault();
-  var target = $($(this));
+  var target = $($(this).attr('href'));
+  console.log(target);
   if(target.length){
-    console.log("entre");
     var scrollTo = target.offset().top;
-    $('body, html').animate({scrollTop: scrollTo+'px'}, 1000);
+    $('body, html').animate({scrollTop: (scrollTo)+'px'}, 1000);
   }
-  console.log("sali");
+});
+
+$(document).on('show','.accordion', function (e) {
+     //$('.accordion-heading i').toggleClass(' ');
+     $(e.target).prev('.accordion-heading').addClass('accordion-opened');
+});
+
+$(document).on('hide','.accordion', function (e) {
+    $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
+    //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
 });
