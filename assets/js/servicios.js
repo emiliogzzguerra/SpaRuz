ecf858c472db33c7e72e177227d7936fae6ec668
emiliogzzguerra <emiliogzz24@gmail.com>
2018-03-18 18:31:38 -0600

    nav y footer mejorado

--- assets/js/servicios.js
+++ assets/js/servicios.js
@@ -7,13 +7,3 @@ $('.subdivision').click(function(e){
     $('body, html').animate({scrollTop: (scrollTo)+'px'}, 1000);
   }
 });
-
-$(document).on('show','.accordion', function (e) {
-     //$('.accordion-heading i').toggleClass(' ');
-     $(e.target).prev('.accordion-heading').addClass('accordion-opened');
-});
-
-$(document).on('hide','.accordion', function (e) {
-    $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
-    //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
-});
